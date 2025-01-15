62f23b379dd9a8db6fa68413486d05fec3486001
Raquel Pau <raquel.pau@broadcom.com>
2025-01-15 12:31:26 +0100

    first commit

---
+++ src/main/java/com/acme/boot/bookings/AcmeBookingsAppApplication.java
@@ -0,0 +1,13 @@
+package com.acme.boot.bookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeBookingsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmeBookingsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/boot/bookings/AcmeBookingsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.boot.bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeBookingsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
